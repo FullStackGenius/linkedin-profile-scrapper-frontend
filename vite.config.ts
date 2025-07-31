cc3faec10a832d4709f13db1773581fdbb961a4a
FullStackGenius <upliftwebdev@gmail.com>
2025-07-31 11:46:08 +0530

    remove port

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-  port: 6001,
-  strictPort: true, // Exits if port 6001 is occupied
-}
 })
